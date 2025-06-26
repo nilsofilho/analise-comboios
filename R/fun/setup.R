fa525902f1ad67c85ddba7a40064ce81e4bbd90c
Nilso de Brito <nilso.britofilho@gmail.com>
2025-06-26 16:29:43 -0300

    added function to create linear reference system (lrs) based on route shape

--- R/fun/setup.R
+++ R/fun/setup.R
@@ -17,7 +17,8 @@ library(stringr)
 library(purrr)
 library(furrr)
 library(future)
-
+library(lubridate)
+library(lwgeom)
 #not in function
 `%nin%` <- Negate(`%in%`)
 
@@ -34,3 +35,26 @@ create_set_ids <- function(sequence, gap_threshold = 2) {
   
   return(set_ids)
 }
+
+
+criar_lrs_preciso <- function(shape_sf, segment_length = 10) {
+  
+  shape_utm <- st_transform(shape_sf, 31984)
+  
+  linha_segmentada <- st_segmentize(shape_utm, units::set_units(segment_length, "m"))
+  
+  lrs_pontos <- st_cast(linha_segmentada, "POINT")
+  
+  dist_segmentos <- st_distance(
+    lrs_pontos[-nrow(lrs_pontos), ], 
+    lrs_pontos[-1, ],             
+    by_element = TRUE
+  )
+  
+  lrs_pontos$dist_acumulada <- c(0, cumsum(as.numeric(dist_segmentos)))
+  
+  return(lrs_pontos)
+}
+
+
+
